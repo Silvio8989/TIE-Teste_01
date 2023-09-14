850709439269fa59b50be6afc8b423566580653f
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2023-09-14 10:12:15 -0300

    Incluindo leitura de comando pela I2C

--- Teste_integracao_v02/Slave/Mux_Entrada/Mux_Entrada.ino
+++ Teste_integracao_v02/Slave/Mux_Entrada/Mux_Entrada.ino
@@ -11,31 +11,29 @@
 #define MUX2 PB1 
 #define MUX3 PA10  
 #define MUX4 PA9 
-#define MUX5 PA8 
+#define MUX5 PA8
 
-void setup() {
+#define LED PC13
 
-  Wire.begin(0X60); Endereço do MUX
+byte comando = 0;
+
+void liga_canal(int canal){
+  switch (canal) {
+    case 0:
+      //Todos os eletrodos desligados
+      digitalWrite(MUX1, HIGH);   
+      digitalWrite(MUX2, HIGH);   
+      digitalWrite(MUX3, HIGH);   
+      digitalWrite(MUX4, HIGH);   
+      digitalWrite(MUX5, HIGH);
   
-  // Variaveis de seleção do Mux
-  pinMode(S0, OUTPUT); 
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(S4, OUTPUT); 
-  
-  //  Variaveis de Habilitação do MUX (Nivel logico baixo)
-  pinMode(MUX1, OUTPUT); // MUX 1 "Eletrodo 1 a 8"
-  pinMode(MUX2, OUTPUT); // MUX 2 "Eletrodo 9 a 16"
-  pinMode(MUX3, OUTPUT); //MUX 3 'Eletrodo 17 a 24"
-  pinMode(MUX4, OUTPUT); // MUX 4 "Eletrodo 25 a 32"
-  pinMode(MUX5, OUTPUT); // MUX 5 " Seleção do MUX
- 
-}
-
-
-void loop() {
- switch () {
+      digitalWrite(S0, LOW);   
+      digitalWrite(S1, LOW);   
+      digitalWrite(S2, LOW);   
+      digitalWrite(S3, LOW);   
+      digitalWrite(S4, LOW);
+      break;
+    
     case 1: 
       //Eletrodo_01
       digitalWrite(MUX1, LOW);   
@@ -97,18 +95,18 @@ void loop() {
       break; 
       
     case 5: 
-       //Eletrodo_05
-       digitalWrite(MUX1, LOW);   
-       digitalWrite(MUX2, HIGH);   
-       digitalWrite(MUX3, HIGH);   
-       digitalWrite(MUX4, HIGH);   
-       digitalWrite(MUX5, LOW);
+      //Eletrodo_05
+      digitalWrite(MUX1, LOW);   
+      digitalWrite(MUX2, HIGH);   
+      digitalWrite(MUX3, HIGH);   
+      digitalWrite(MUX4, HIGH);   
+      digitalWrite(MUX5, LOW);
       
-       digitalWrite(S0, LOW);   
-       digitalWrite(S1, LOW);   
-       digitalWrite(S2, HIGH);   
-       digitalWrite(S3, LOW);   
-       digitalWrite(S4, LOW);
+      digitalWrite(S0, LOW);   
+      digitalWrite(S1, LOW);   
+      digitalWrite(S2, HIGH);   
+      digitalWrite(S3, LOW);   
+      digitalWrite(S4, LOW);
       break; 
 
     case 6:
@@ -124,7 +122,8 @@ void loop() {
       digitalWrite(S2, HIGH);   
       digitalWrite(S3, LOW);   
       digitalWrite(S4, LOW); 
-    break;
+      break;
+    
     case 7:
       //Eletrodo_07  
       digitalWrite(MUX1, LOW);   
@@ -138,21 +137,65 @@ void loop() {
       digitalWrite(S2, HIGH);   
       digitalWrite(S3, LOW);   
       digitalWrite(S4, LOW); 
-    break;
+      break;
+    
     case 8:
-       //Eletrodo_08
-       digitalWrite(MUX1, LOW);   
-       digitalWrite(MUX2, HIGH);   
-       digitalWrite(MUX3, HIGH);   
-       digitalWrite(MUX4, HIGH);   
-       digitalWrite(MUX5, LOW);
-      
-       digitalWrite(S0, HIGH);   
-       digitalWrite(S1, HIGH);   
-       digitalWrite(S2, HIGH);   
-       digitalWrite(S3, LOW);   
-       digitalWrite(S4, LOW); 
-    break; 
+      //Eletrodo_08
+      digitalWrite(MUX1, LOW);   
+      digitalWrite(MUX2, HIGH);   
+      digitalWrite(MUX3, HIGH);   
+      digitalWrite(MUX4, HIGH);   
+      digitalWrite(MUX5, LOW);
+     
+      digitalWrite(S0, HIGH);   
+      digitalWrite(S1, HIGH);   
+      digitalWrite(S2, HIGH);   
+      digitalWrite(S3, LOW);   
+      digitalWrite(S4, LOW); 
+      break; 
+    
     default:
       break;
+   }
+}
+
+void configura_pinos_mux(){
+  // Variaveis de seleção do Mux
+  pinMode(S0, OUTPUT); 
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(S4, OUTPUT); 
+  
+  //  Variaveis de Habilitação do MUX (Nivel logico baixo)
+  pinMode(MUX1, OUTPUT); // MUX 1 "Eletrodo 1 a 8"
+  pinMode(MUX2, OUTPUT); // MUX 2 "Eletrodo 9 a 16"
+  pinMode(MUX3, OUTPUT); //MUX 3 'Eletrodo 17 a 24"
+  pinMode(MUX4, OUTPUT); // MUX 4 "Eletrodo 25 a 32"
+  pinMode(MUX5, OUTPUT); // MUX 5 " Seleção do MUX
+}
+
+void dadorecebido(int howmany){
+  comando = Wire.read();  
+  if(comando == 0) comando = 0xFF;
+}
+
+void processacomando(){
+  digitalWrite(LED, LOW); // Verificação de funcionamento
+  if(comando < 0xFF) liga_canal(comando);
+  else liga_canal(0);
+  comando = 0;
+  dela(2); // para ver o LED acesso
+  digitalWrite(LED, HIGH); // Verificação de funcionamento
+}
+
+void setup() {
+  Wire.begin(0X60); Endereço do MUX
+  Wire.onReceive(dadorecebido);    // register event
+  configura_pinos_mux();
+  liga_canal(0);
+}
+
+void loop() {
+  if(comando!=0) processacomando();
 }
